0a3f482d4678913c4cbdc52e92eaedc554fbe082
Eliel Santos Mumbarra <elielsantosm.2015@gmail.com>
2024-07-19 08:32:15 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddRouting(option => option.LowercaseUrls = true);
+
+var test = builder.Configuration.GetValue<int>("MyClass:Number");
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ livro.cs
@@ -0,0 +1,14 @@
+﻿using MyFirstApi.Enum;
+
+namespace MyFirstApi
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public GeneroLivro Genero { get; set; }
+        public decimal Preco { get; set; }
+        public int QuantidadeEstoque { get; set; }
+    }
+}
